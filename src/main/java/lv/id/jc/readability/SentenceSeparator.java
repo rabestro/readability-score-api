aa97bc0187732bd842620aea867ea064c089a963
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-12-09 17:33:13 +0200

    RSG-4 Add SentenceSeparator class and test cases
    Introduced new SentenceSeparator class that implements the TextToSentencesFunction interface. This class is intended to split a text into sentences. Also, added test cases in the SentencesSeparatorSpec.groovy to verify the functionality. Further development is needed as the SentenceSeparator functionality is currently unimplemented.

---
+++ src/main/java/lv/id/jc/readability/SentenceSeparator.java
@@ -0,0 +1,12 @@
+package lv.id.jc.readability;
+
+import java.util.stream.Stream;
+
+
+public final class SentenceSeparator implements TextToSentencesFunction {
+
+    @Override
+    public Stream<String> apply(String s) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
